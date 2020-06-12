71607c3caf4abf208a17cf3f8290ae36f8a50ffe
Harsh Shah <harshbnshah@gmail.com>
2020-06-13 03:35:01 +0530

    created the project with create react app and added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import './homepage.styles.scss';
+import { motion } from "framer-motion";
+
+const HomePage = () => (
+  <div className="homepage">
+    <motion.div className="directory-menu">
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      <motion.div
+        className="menu-item"
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="content">
+          <h1 className="title">CATEGORY 1</h1>
+          <span className="subtitle">EXPLORE NOW</span>
+        </div>
+      </motion.div>
+      
+    </motion.div>
+  </div>
+);
+
+export default HomePage;
