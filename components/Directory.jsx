ffec98264609e67b95480658fbbf85c46a5ec3e5
vignejs <vignesh.umashankar@gmail.com>
2020-06-15 21:48:53 +0530

    Finished Design

--- components/Directory.jsx
+++ components/Directory.jsx
@@ -8,33 +8,28 @@ const sections = [
     title: "Technology",
     imageUrl:
       "/images/technology-christina-wocintechchat-com-glRqyWJgUeY-unsplash.jpg",
-    id: 1,
   },
   {
-    title: "Finance/Accounting",
+    title: "Finance",
     imageUrl: "/images/finance-adeolu-eletu-unRkg2jH1j0-unsplash.jpg",
-    id: 2,
   },
   {
     title: "Education",
     imageUrl: "/images/education-annie-spratt-4-4WPFLVhAY-unsplash.jpg",
-    id: 3,
   },
   {
     title: "Designing",
     imageUrl: "/images/design-convertkit-gLWT-VqMG20-unsplash.jpg",
-    id: 4,
   },
   {
     title: "Architecture",
     imageUrl: "/images/architecture-lance-anderson-QdAAasrZhdk-unsplash.jpg",
-    id: 5,
   },
 ];
 
 export const Directory = () => (
   <div css={tw`max-w-screen-lg p-10 mx-auto`}>
-    <h1 css={tw`font-bold px-3 text-2xl text-gray-900`}>Categories:</h1>
+    <h1 css={tw`font-bold px-4 text-3xl`}>Categories</h1>
     <div css={tw`flex flex-row flex-wrap box-border`}>
       {sections.map(({ title, imageUrl }, id) => (
         <MenuCard key={id} title={title} imageUrl={imageUrl} />
--- components/MenuCard.jsx
+++ components/MenuCard.jsx
@@ -2,32 +2,85 @@
 import { jsx } from "@emotion/core";
 import { motion } from "framer-motion";
 import tw, { styled, css } from "twin.macro";
+import { useState, useEffect } from "react";
 
-const BackgroundImage = styled.div`
-  ${tw`h-64 w-full rounded-xl`}
-  background-position: center;
-  background-size: cover;
-  background-image: url("${(props) => props.url}");
-`;
+const BackgroundImage = ({ isHovered, url }) => {
+  const spring = {
+    ease: "easeOut",
+    duration: 0.2,
+  };
+  return (
+    <motion.div
+      css={[
+        css`background-position: center;
+        background-size: cover;
+        background-image: url("${url}");`,
+        isHovered
+          ? css`
+              &::before {
+                background-color: rgb(240, 14, 46);
+                content: "";
+                display: block;
+                border-radius: 12px;
+                width: 100%;
+                height: 100%;
+                mix-blend-mode: darken;
+                position: absolute;
+                top: 0;
+                left: 0;
+              }
+              &::after {
+                background-color: rgb(25, 37, 80);
+                content: "";
+                display: block;
+                width: 100%;
+                height: 100%;
+                border-radius: 12px;
+                mix-blend-mode: lighten;
+                position: absolute;
+                top: 0;
+                left: 0;
+              }
+            `
+          : css``,
+        tw`h-56 w-full rounded-xl`,
+      ]}
+      transition={spring}
+      animate={{
+        height: isHovered ? "16rem" : "14rem",
+        y: isHovered ? "0rem" : "2rem",
+      }}
+    ></motion.div>
+  );
+};
 
-export const MenuCard = ({ title, imageUrl }, id) => {
+export const MenuCard = ({ title, imageUrl }) => {
+  const [isHovered, setHovered] = useState(false);
   return (
     <motion.div
       css={[
         css`
           flex-basis: 25%;
         `,
-        tw`rounded-xl cursor-pointer flex-grow flex-shrink box-border m-3 justify-between`,
+        tw`h-64 rounded-xl cursor-pointer flex-grow flex-shrink box-border m-3 justify-between`,
       ]}
-      key={id}
       whileHover={{
-        "flex-basis": "35%",
+        "flex-basis": "30%",
       }}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
     >
-      <div className="content">
-        <h1 css={tw`font-bold pb-3 px-1 text-xl text-gray-900`}>{title}</h1>
-      </div>
-      <BackgroundImage url={imageUrl} />
+      <motion.div
+        css={[
+          tw`absolute z-10 font-bold pb-3 px-1 text-xl`,
+          isHovered
+            ? tw`h-64 text-white text-4xl font-black px-5 py-2`
+            : tw``,
+        ]}
+      >
+        {title}
+      </motion.div>
+      <BackgroundImage isHovered={isHovered} url={imageUrl} />
     </motion.div>
   );
 };
